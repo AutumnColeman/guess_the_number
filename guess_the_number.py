10d9ff24bf87f3e88b42387fb0bbc2f3912a8b63
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-09-22 14:13:50 -0400

    Added game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,26 +1,10 @@
-import random
-secret_number = random.randint(1, 10)
-tries = 5
-play_again = "y"
+print "I am thinking of a number between 1 and 10."
+secret_number = 5
+guess = int(raw_input("What's the number? "))
 
-while play_again != "n":
-    guess = int(raw_input("I am thinking of a number between 1 and 10. What's the number? "))
-    while (guess != secret_number) and tries > 1:
-        if guess > secret_number:
-            tries = tries - 1
-            print ("%d is too high.") % guess
+while (guess != 5):
+    print "Nope, try again."
+    guess = int(raw_input("What's the number? "))
 
-        elif guess < secret_number and tries > 1:
-            tries = tries - 1
-            print ("%d is too low") % guess
-
-        print ("You have %d guesses left") % tries
-        guess = int(raw_input("What's the number? "))
-
-    if guess != secret_number:
-                print "You ran out of guesses!"
-
-    else:
-            print "Yes! You win!"
-
-    play_again = (raw_input("Do you want to play again (Y or N)? ")).lower()
+else:
+    print "Yes! You win!"
