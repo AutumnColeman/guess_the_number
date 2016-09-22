369664229011bcdde730509afff21c95ceaaea4c
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-09-22 14:09:54 -0400

    Added random num generator to game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,13 +1,13 @@
-print "I am thinking of a number between 1 and 10."
-secret_number = 5
-guess = int(raw_input("What's the number? "))
+import random
+secret_number = random.randint(1, 10)
+guess = int(raw_input("I am thinking of a number between 1 and 10. What's the number? "))
 
-while (guess != 5):
-    if guess > 5:
+while (guess != secret_number):
+    if guess > secret_number:
         print ("%d is too high.") % guess
         guess = int(raw_input("What's the number? "))
 
-    elif guess < 5:
+    elif guess < secret_number:
         print ("%d is too low") % guess
         guess = int(raw_input("What's the number? "))
 
