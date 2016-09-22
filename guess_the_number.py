55c99790abfd6a4f02513d026a00e937b50ccf95
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-09-22 14:10:27 -0400

    Added attempt limit to game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,26 @@
 import random
 secret_number = random.randint(1, 10)
-guess = int(raw_input("I am thinking of a number between 1 and 10. What's the number? "))
+tries = 5
+play_again = "y"
 
-while (guess != secret_number):
-    if guess > secret_number:
-        print ("%d is too high.") % guess
+while play_again != "n":
+    guess = int(raw_input("I am thinking of a number between 1 and 10. What's the number? "))
+    while (guess != secret_number) and tries > 1:
+        if guess > secret_number:
+            tries = tries - 1
+            print ("%d is too high.") % guess
+
+        elif guess < secret_number and tries > 1:
+            tries = tries - 1
+            print ("%d is too low") % guess
+
+        print ("You have %d guesses left") % tries
         guess = int(raw_input("What's the number? "))
 
-    elif guess < secret_number:
-        print ("%d is too low") % guess
-        guess = int(raw_input("What's the number? "))
+    if guess != secret_number:
+                print "You ran out of guesses!"
 
-else:
-    print "Yes! You win!"
+    else:
+            print "Yes! You win!"
+
+    play_again = (raw_input("Do you want to play again (Y or N)? ")).lower()
